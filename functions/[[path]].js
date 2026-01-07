790dc296860bc53895ae4520c074e56c44534d29
Pedro Henrique Martins <doomicus.dev@gmail.com>
2026-01-07 11:47:44 -0300

    adicioando o carai denovo

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -2,51 +2,53 @@ export async function onRequest(context) {
   const { request, env } = context;
   const url = new URL(request.url);
 
-  // Rota de autorização
-  if (url.pathname === "/oauth/authorize") {
-    const redirectUrl = new URL("https://github.com/login/oauth/authorize");
-    redirectUrl.searchParams.set("client_id", env.GITHUB_CLIENT_ID);
-    redirectUrl.searchParams.set("scope", "repo,user");
-    return Response.redirect(redirectUrl.href, 302);
+  if (
+    url.pathname === "/oauth/authorize" ||
+    url.pathname === "/oauth/callback"
+  ) {
+    if (url.pathname === "/oauth/authorize") {
+      const redirectUrl = new URL("https://github.com/login/oauth/authorize");
+      redirectUrl.searchParams.set("client_id", env.GITHUB_CLIENT_ID);
+      redirectUrl.searchParams.set("scope", "repo,user");
+      return Response.redirect(redirectUrl.href, 302);
+    }
+
+    if (url.pathname === "/oauth/callback") {
+      const code = url.searchParams.get("code");
+      const response = await fetch(
+        "https://github.com/login/oauth/access_token",
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+            "user-agent": "cloudflare-pages-consumer",
+            accept: "application/json",
+          },
+          body: JSON.stringify({
+            client_id: env.GITHUB_CLIENT_ID,
+            client_secret: env.GITHUB_CLIENT_SECRET,
+            code,
+          }),
+        }
+      );
+      const result = await response.json();
+
+      const content = `
+        <script>
+          const receiveMessage = (message) => {
+            window.opener.postMessage(
+              'authorization:github:success:${JSON.stringify(result)}',
+              message.origin
+            );
+          };
+          window.addEventListener("message", receiveMessage, false);
+          window.opener.postMessage("authorizing:github", "*");
+        </script>`;
+      return new Response(content, {
+        headers: { "content-type": "text/html" },
+      });
+    }
   }
 
-  // Rota de callback
-  if (url.pathname === "/oauth/callback") {
-    const code = url.searchParams.get("code");
-    const response = await fetch(
-      "https://github.com/login/oauth/access_token",
-      {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-          "user-agent": "cloudflare-pages-consumer",
-          accept: "application/json",
-        },
-        body: JSON.stringify({
-          client_id: env.GITHUB_CLIENT_ID,
-          client_secret: env.GITHUB_CLIENT_SECRET,
-          code,
-        }),
-      }
-    );
-    const result = await response.json();
-
-    // Script para enviar o token de volta para o Decap CMS
-    const content = `
-      <script>
-        const receiveMessage = (message) => {
-          window.opener.postMessage(
-            'authorization:github:success:${JSON.stringify(result)}',
-            message.origin
-          );
-        };
-        window.addEventListener("message", receiveMessage, false);
-        window.opener.postMessage("authorizing:github", "*");
-      </script>`;
-    return new Response(content, { headers: { "content-type": "text/html" } });
-  }
-
-  return Object.assign(new Response("Not Found", { status: 404 }), {
-    statusText: "Not Found",
-  });
+  return env.ASSETS.fetch(request);
 }
